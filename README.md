7202e8d0bfa7e640ed9051d14e66895179ccf495
Mahmudur <mahmudhera93@gmail.com>
2024-10-29 12:41:16 -0400

    added results for this

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ This program will do the following:
 1. Generate the sketch of A using FMH and AS
 1. Record the sketch sizes
 1. Vary x from 1K to 10M
+
+Results:
+
+```
+10000,10.2,2.6,573,8.82043
+100000,96.5,7.32462,790.3,31.8624
+1000000,1004.2,26.5548,1021,30.4335
+10000000,9968.3,73.369,1247,42.1829
+100000000,97035.5,302.213,1486.3,24.0626
+```
--- sketchSizeTest.cpp
+++ sketchSizeTest.cpp
@@ -15,14 +15,17 @@ using namespace std;
 struct Arguments {
     uint scaled;
     uint k;
+    uint num_trials;
 };
-
 
 typedef Arguments Arguments;
 
 Arguments arguments;
 
-
+/*
+This function parses the arguments, and sets the global struct arguments.
+The plan is to make this struct available to all functions in the program.
+*/
 void parse_arguments(int argc, char* argv[]) {
     // Define the parser
     argparse::ArgumentParser program("sketchSizeTest", "0.0.0");
@@ -39,6 +42,13 @@ void parse_arguments(int argc, char* argv[]) {
         .help("Value for k in AffirmativeSketch")
         .scan<'i', int>()
         .default_value(100);
+
+    // number of trials in each setting
+    program.add_argument("-n", "--num_trials")
+        .help("Number of trials in each setting")
+        .scan<'i', int>()
+        .default_value(20);
+
         
     // Parse the arguments
     try {
@@ -51,6 +61,7 @@ void parse_arguments(int argc, char* argv[]) {
     // set the arguments in the global struct
     arguments.scaled = program.get<int>("--scaled");
     arguments.k = program.get<int>("--k");
+    arguments.num_trials = program.get<int>("--num_trials");
 
 }
 
@@ -63,6 +74,70 @@ int main(int argc, char* argv[]) {
     // show arguments from the global struct
     cout << "Scaled: " << arguments.scaled << endl;
     cout << "k: " << arguments.k << endl;
+
+    vector<int> sizes = {10000, 100000, 1000000, 10000000, 100000000};
+    int max_size = 1000000000;
+    cout << "Sizes of the original sets considered: " << endl;
+    for (int size : sizes) {
+        cout << size << " ";
+    }
+    cout << endl;
+
+    for (int size : sizes) {
+
+        vector<int> fmh_sketch_sizes;
+        vector<int> aff_sketch_sizes;
+
+        for (int i = 0; i < arguments.num_trials; i++) {
+
+            // create a set of size 'size' randomly
+            vector<int> original_set;
+            for (int i = 0; i < size; i++) {
+                original_set.push_back( rand() % max_size );
+            }
+
+            // create sketches
+            FracMinHashSketch fmh_sketch(arguments.scaled);
+            AffirmativeSketch aff_sketch(arguments.k);
+            
+            for (int value : original_set) {
+                auto hash_value = mmh3(&value, sizeof(value), 0);
+                fmh_sketch.add(hash_value);
+                aff_sketch.add(hash_value);
+            }
+
+            fmh_sketch_sizes.push_back(fmh_sketch.get().size());
+            aff_sketch_sizes.push_back(aff_sketch.get().size());
+
+        }
+
+        // print the average and std of the sizes of the sketches
+        double fmh_avg = 0;
+        double aff_avg = 0;
+        for (int i = 0; i < arguments.num_trials; i++) {
+            fmh_avg += fmh_sketch_sizes[i];
+            aff_avg += aff_sketch_sizes[i];
+        }
+
+        fmh_avg /= arguments.num_trials;
+        aff_avg /= arguments.num_trials;
+
+        double fmh_std = 0;
+        double aff_std = 0;
+
+        for (int i = 0; i < arguments.num_trials; i++) {
+            fmh_std += (fmh_sketch_sizes[i] - fmh_avg) * (fmh_sketch_sizes[i] - fmh_avg);
+            aff_std += (aff_sketch_sizes[i] - aff_avg) * (aff_sketch_sizes[i] - aff_avg);
+        }
+
+        fmh_std = sqrt(fmh_std / arguments.num_trials);
+        aff_std = sqrt(aff_std / arguments.num_trials);
+
+        cout << size << "," << fmh_avg << "," << fmh_std << "," << aff_avg << "," << aff_std << endl;
+
+
+    }
+
 
     return 0;
 }
