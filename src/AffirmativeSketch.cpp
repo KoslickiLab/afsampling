cd2eec2bd3f3b5d14e6008d5e05482f607ce46f7
Mahmudur <mahmudhera93@gmail.com>
2024-10-31 23:31:09 -0400

    implement small hashes (not large)

--- src/AffirmativeSketch.cpp
+++ src/AffirmativeSketch.cpp
@@ -16,22 +16,28 @@ void AffirmativeSketch::add(hash_t value) {
     // for the first k elements, add them to the sketch
     if (data_.size() < k) {
         Sketch::add(value);
-        threshold1_ = threshold2_ = data_.begin()->first;
+        // get the last element as the thresholds
+        auto it = data_.rbegin();
+        threshold1_ = it->first;
+        threshold2_ = it->first;
     
+    // ------ acceptance region1 ---- | ---- acceptance region2 ---- | ---- rejection region ----
+    // -------------------------- threshold2 ------------------ threshold1 ----------------------
+
     // for the rest:
     } else {
-        if (value < threshold1_) {
-            // if < threshold1, then do not add to sketch
+        if (value > threshold1_) {
+            // if > threshold1, then do not add to sketch
             return;
-        } else if (value < threshold2_) {
-            // if < threshold2, add to sketch and remove the first element
+        } else if (value > threshold2_) {
+            // if > threshold2, add to sketch and remove the first element
             Sketch::add(value);
-            data_.erase(data_.begin());
-            threshold1_ = data_.begin()->first;
+            data_.erase(data_.rbegin()->first);
+            threshold1_ = data_.rbegin()->first;
         } else {
-            // if > threshold2, add to sketch and update threshold2 = the kth largest element
+            // if <= threshold2, add to sketch and update threshold2 = the kth smallest element
             Sketch::add(value);
-            auto it = data_.rbegin();
+            auto it = data_.begin();
             std::advance(it, k - 1);
             threshold2_ = it->first;
         }
@@ -54,19 +60,22 @@ double AffirmativeSketch::jaccard(const Sketch& other) const {
     std::vector<hash_t> this_sketch = get();
     std::vector<hash_t> other_sketch = other.get();
 
+    size_t this_size = this_sketch.size();
+    size_t other_size = other_sketch.size();
+
     // get the smallest common hash
-    size_t i = 0, j = 0;
+    size_t i = this_size - 1, j = other_size - 1;
     bool found = false;
-    hash_t smallest_common_hash = -1;
-    while (i < this_sketch.size() && j < other_sketch.size()) {
+    hash_t largest_common_hash = -1;
+    while (i > 0 && j > 0) {
         if (this_sketch[i] == other_sketch[j]) {
             found = true;
-            smallest_common_hash = this_sketch[i];
+            largest_common_hash = this_sketch[i];
             break;
-        } else if (this_sketch[i] < other_sketch[j]) {
-            i++;
+        } else if (this_sketch[i] > other_sketch[j]) {
+            i--;
         } else {
-            j++;
+            j--;
         }
     }
 
@@ -77,22 +86,22 @@ double AffirmativeSketch::jaccard(const Sketch& other) const {
 
     std::set<hash_t> union_set;
     for (hash_t value : this_sketch) {
-        if (value < smallest_common_hash) {
-            continue;
+        if (value > largest_common_hash) {
+            break;
         }
         union_set.insert(value);
     }
     for (hash_t value : other_sketch) {
-        if (value < smallest_common_hash) {
-            continue;
+        if (value > largest_common_hash) {
+            break;
         }
         union_set.insert(value);
     }
 
     std::set<hash_t> intersection_set;
     for (hash_t value : this_sketch) {
-        if (value < smallest_common_hash) {
-            continue;
+        if (value > largest_common_hash) {
+            break;
         }
         if (std::find(other_sketch.begin(), other_sketch.end(), value) != other_sketch.end()) {
             intersection_set.insert(value);
